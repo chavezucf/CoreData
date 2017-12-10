e380d35863ff2ac28cf9d89f8fc20a0d77622968
Miguel <mac3@knights.ucf.edu>
2017-12-10 10:15:34 -0500

    Init Commit
    New Xcode Project

---
+++ CoreDataTraning/CoreDataTraning/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataTraning
+//
+//  Created by Dev Miguel Chavez on 12/10/17.
+//  Copyright © 2017 Dev Miguel Chavez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
